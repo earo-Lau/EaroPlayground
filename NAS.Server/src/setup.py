d6d0e7171796d816ea64cd63c0fba02158a3c7dc
lauearo <earo.lau@outlook.com>
2018-09-08 12:07:01 +0800

    setup script

---
+++ NAS.Server/src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NAS.Server',
+    version='Beta 0.1',
+    packages=['', 'Model', 'APIController', 'UploadService', 'HttpMiddleware'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='lauearo',
+    author_email='',
+    description=''
+)
